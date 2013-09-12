68ecb94bbe92704bcdec8608527472d35d78b0ff
myselph <huberte@microsoft.com>
2013-09-12 04:14:59 -0700

    added multithreading support

--- nativeconsole.h
+++ nativeconsole.h
@@ -1,9 +1,13 @@
 #include <v8.h>
 
+using namespace v8;
+void logUvAsyncCb(uv_async_t *, int);
+void infoUvAsyncCb(uv_async_t *, int);
+void warnUvAsyncCb(uv_async_t *, int);
+void errorUvAsyncCb(uv_async_t *, int);
 
 namespace NodeUtils
 {
-  using namespace v8;
 
   class Console 
   {
@@ -11,6 +15,7 @@ namespace NodeUtils
     Console()
     {
       ConnectToJSConsole();
+      SetupThreadMessageReceivers();
     }
 
     ~Console()
@@ -18,14 +23,68 @@ namespace NodeUtils
       Dispose();
     }
 
-    void Log(Handle<String> str) { Log(_log, str); }
-    void Log(const wchar_t *wstr) { Log(_log, wstr); }
-    void Info(Handle<String> str) { Log(_info, str); }
-    void Info(const wchar_t *wstr) { Log(_info, wstr); }
-    void Warn(Handle<String> str) { Log(_warn, str); }
-    void Warn(const wchar_t *wstr) { Log(_warn, wstr); }
-    void Error(Handle<String> str) { Log(_error, str); }
-    void Error(const wchar_t *wstr) { Log(_error, wstr); }
+    void Log(Handle<String> str)
+    {
+      if (uvThreadID != GetCurrentThreadId()) return; // other threads may not pass in Handles (like, Local<>)
+      Log(_log, str); 
+    }
+    
+    void Log(const wchar_t* data)
+    {
+      if (uvThreadID == GetCurrentThreadId()) {
+        Log(_log, String::New(data));
+      } else {
+        _logUvAsync.data = (void *)data;
+        uv_async_send(&_logUvAsync);
+      }
+    }
+
+    void Info(Handle<String> str)
+    {
+      if (uvThreadID != GetCurrentThreadId()) return;
+      Log(_info, str); 
+    }
+    
+    void Info(const wchar_t* str)
+    {
+      if (uvThreadID == GetCurrentThreadId()) {
+        Log(_info, String::New(str));
+      } else {
+        _infoUvAsync.data = (void *)str;
+        uv_async_send(&_infoUvAsync);
+      }
+    }
+
+    void Warn(Handle<String> str)
+    {
+      if (uvThreadID != GetCurrentThreadId()) return; 
+      Log(_warn, str); 
+    }
+    
+    void Warn(const wchar_t* str)
+    {
+      if (uvThreadID == GetCurrentThreadId()) {
+        Log(_warn, String::New(str));
+      } else {
+        _warnUvAsync.data = (void *)str;
+        uv_async_send(&_warnUvAsync);
+      }
+    }
+
+    void Error(Handle<String> str)
+    {
+      if (uvThreadID != GetCurrentThreadId()) return;
+      Log(_error, str); 
+    }
+    void Error(const wchar_t* str)
+    {
+      if (uvThreadID == GetCurrentThreadId()) {
+        Log(_error, String::New(str));
+      } else {
+        _errorUvAsync.data = (void *)str;
+        uv_async_send(&_errorUvAsync);
+      }
+    }
 
     void ConnectToJSConsole()
     {
@@ -41,6 +100,15 @@ namespace NodeUtils
         _warn = Persistent<Function>::New(_console->Get(String::NewSymbol("warn")).As<Function>());
         _error = Persistent<Function>::New(_console->Get(String::NewSymbol("error")).As<Function>());
       }
+    }
+    
+    void SetupThreadMessageReceivers()
+    {
+      uv_async_init(uv_default_loop(), &_logUvAsync, logUvAsyncCb);
+      uv_async_init(uv_default_loop(), &_infoUvAsync, infoUvAsyncCb);
+      uv_async_init(uv_default_loop(), &_warnUvAsync, warnUvAsyncCb);
+      uv_async_init(uv_default_loop(), &_errorUvAsync, errorUvAsyncCb);
+      uvThreadID = GetCurrentThreadId(); // the constructor is being called from the main (event-loop) thread.
     }
 
   private:
@@ -78,5 +146,36 @@ namespace NodeUtils
     Persistent<Function> _info;
     Persistent<Function> _warn;
     Persistent<Function> _error;
+    DWORD uvThreadID;
+    uv_async_t _logUvAsync;
+    uv_async_t _infoUvAsync;
+    uv_async_t _warnUvAsync;
+    uv_async_t _errorUvAsync;
   };
 }
+
+//needed for functions below
+static NodeUtils::Console console;
+
+/* Helper functions running in the main thread that receive the 
+ * string to be printed from a different thread */
+void logUvAsyncCb(uv_async_t *handle, int status)
+{
+	console.Log(String::New((wchar_t *)(handle->data)));
+}
+
+void infoUvAsyncCb(uv_async_t *handle, int status)
+{
+	console.Info(String::New((wchar_t *)(handle->data)));
+}
+
+void warnUvAsyncCb(uv_async_t *handle, int status)
+{
+	console.Warn(String::New((wchar_t *)(handle->data)));
+}
+
+void errorUvAsyncCb(uv_async_t *handle, int status)
+{
+	console.Error(String::New((wchar_t *)(handle->data)));
+}
+
