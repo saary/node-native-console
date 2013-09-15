9250cfffc93c35acf48da78b797981daa3f5d1ee
Saar Yahalom <saary@microsoft.com>
2013-09-15 23:30:16 +0300

    use node-async to run on main thread

--- nativeconsole.h
+++ nativeconsole.h
@@ -3,6 +3,7 @@
 #include <v8.h>
 #include <uv.h>
 #include <functional>
+#include "node-async.h"
 
 namespace NodeUtils
 {
@@ -14,7 +15,6 @@ namespace NodeUtils
     Console()
     {
       ConnectToJSConsole();
-      _uvMain = uv_thread_self();
     }
 
     ~Console()
@@ -23,10 +23,10 @@ namespace NodeUtils
     }
 
     // may be called from main thread or worker threads / RT event threads
-    void Log(const wchar_t *wstr) { RunOnMain(new std::function<void ()>([&] { Log(_log, wstr); })); }
-    void Info(const wchar_t *wstr) { RunOnMain(new std::function<void ()>([&] { Log(_info, wstr); })); }
-    void Warn(const wchar_t *wstr) { RunOnMain(new std::function<void ()>([&] { Log(_warn, wstr); })); }
-    void Error(const wchar_t *wstr) { RunOnMain(new std::function<void ()>([&] { Log(_error, wstr); })); }
+    void Log(const wchar_t *wstr) { Async::RunOnMain([&] { Log(_log, wstr); }); }
+    void Info(const wchar_t *wstr) { Async::RunOnMain([&] { Log(_info, wstr); }); }
+    void Warn(const wchar_t *wstr) { Async::RunOnMain([&] { Log(_warn, wstr); }); }
+    void Error(const wchar_t *wstr) { Async::RunOnMain([&] { Log(_error, wstr); }); }
 
     void ConnectToJSConsole()
     {
@@ -73,37 +73,12 @@ namespace NodeUtils
       }
     }
 
-    void RunOnMain(std::function<void ()> *func)
-    {
-      if (_uvMain == uv_thread_self()) 
-      {
-        (*func)();
-        delete func;
-      }
-      else
-      {
-        uv_async_t *async = new uv_async_t;
-        uv_async_init(uv_default_loop(), async, AsyncCb);
-        async->data = func;
-        uv_async_send(async);
-      }
-    }
-
-    static void AsyncCb(uv_async_t *handle, int status)
-    {
-      auto func = static_cast<std::function<void ()>*>(handle->data);
-      (*func)();
-      delete func;
-      delete handle;
-    }
-
   private:
     Persistent<Object> _console;
     Persistent<Function> _log;
     Persistent<Function> _info;
     Persistent<Function> _warn;
     Persistent<Function> _error;
-    unsigned long _uvMain;
   };
 }
 
---
+++ node-async.h
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <v8.h>
+#include <functional>
+#include <memory>
+#include <TlHelp32.h>
+
+namespace NodeUtils
+{
+  using namespace v8;
+
+  class Async
+  {
+  public:
+    template<typename TInput, typename TResult> 
+    struct Baton {
+      int error_code;
+      std::string error_message;
+
+      // Custom data
+      std::shared_ptr<TInput> data;
+      std::shared_ptr<TResult> result;
+
+    private:
+      uv_work_t request;
+      std::function<void (Baton*)> doWork;
+      std::function<void (Handle<Function> callback, Baton*)> afterWork;
+      Persistent<Function> callback;
+
+      friend Async;
+    };
+
+    template<typename TInput, typename TResult> 
+    static void Run(
+      std::shared_ptr<TInput> input, 
+      std::function<void (Baton<TInput, TResult>*)> doWork, 
+      std::function<void (Handle<Function> callback, Baton<TInput, TResult>*)> afterWork, 
+      Handle<Function> callback)
+    {
+      HandleScope scope;
+
+      Baton<TInput, TResult>* baton = new Baton<TInput, TResult>();
+      baton->request.data = baton;
+      baton->callback = Persistent<Function>::New(callback);
+      baton->error_code = 0;
+      baton->data = input;
+      baton->doWork = doWork;
+      baton->afterWork = afterWork;
+
+      uv_queue_work(uv_default_loop(), &baton->request, AsyncWork<TInput, TResult>, AsyncAfter<TInput, TResult>);
+    }
+
+    static void RunOnMain(
+      std::function<void ()> func)
+    {
+      static unsigned int uvMainThreadId = GetMainThreadId();
+
+      if (uvMainThreadId == uv_thread_self()) 
+      {
+        func();
+      }
+      else
+      {
+        std::function<void ()> *funcPtr = new std::function<void ()>(func);
+        uv_async_t *async = new uv_async_t;
+        uv_async_init(uv_default_loop(), async, AsyncCb);
+        async->data = funcPtr;
+        uv_async_send(async);
+      }
+    }
+
+  private:
+    template<typename TInput, typename TResult> 
+    static void AsyncWork(uv_work_t* req) 
+    {
+      // No HandleScope!
+
+      Baton<TInput, TResult>* baton = static_cast<Baton<TInput, TResult>*>(req->data);
+
+      // Do work in threadpool here.
+      // Set baton->error_code/message on failures.
+      // Set baton->result with a final result object
+      baton->doWork(baton);
+    }
+
+    template<typename TInput, typename TResult> 
+    static void AsyncAfter(uv_work_t* req, int status) 
+    {
+      HandleScope scope;
+      Baton<TInput, TResult>* baton = static_cast<Baton<TInput, TResult>*>(req->data);
+
+      // typical AfterWorkFunc implementation
+      //if (baton->error) 
+      //{
+      //    // Call callback with error object.
+      //} 
+      //else 
+      //{
+      //    // Call callback with results.
+      //}
+
+      baton->afterWork(baton->callback, baton);
+
+      baton->callback.Dispose();
+      delete baton;
+    }
+
+    // Called by run on main in case we are not running on the main thread
+    static void AsyncCb(uv_async_t *handle, int status)
+    {
+      auto func = static_cast<std::function<void ()>*>(handle->data);
+      (*func)();
+      delete func;
+      delete handle;
+    }
+
+    static unsigned int GetMainThreadId() 
+    {
+      const std::shared_ptr<void> hThreadSnapshot(
+        CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0), CloseHandle);
+
+      if (hThreadSnapshot.get() == INVALID_HANDLE_VALUE)
+      {
+        return 0;
+      }
+
+      THREADENTRY32 tEntry;
+      tEntry.dwSize = sizeof(THREADENTRY32);
+      DWORD result = 0;
+      DWORD currentPID = GetCurrentProcessId();
+
+      for (BOOL success = Thread32First(hThreadSnapshot.get(), &tEntry);
+        !result && success && GetLastError() != ERROR_NO_MORE_FILES;
+        success = Thread32Next(hThreadSnapshot.get(), &tEntry))
+      {
+        if (tEntry.th32OwnerProcessID == currentPID) 
+        {
+          result = tEntry.th32ThreadID;
+        }
+      }
+      return result;
+    }
+  };
+}
